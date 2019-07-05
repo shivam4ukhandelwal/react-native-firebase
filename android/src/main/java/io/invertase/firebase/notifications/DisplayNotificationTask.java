d3bfbe99b6d38db5cffbc9af255fa4f1f7a4d3f3
shivam khandelwal <3694429+shivam4ukhandelwal@users.noreply.github.com>
2019-07-05 14:16:16 +0530

    Update DisplayNotificationTask.java

--- android/src/main/java/io/invertase/firebase/notifications/DisplayNotificationTask.java
+++ android/src/main/java/io/invertase/firebase/notifications/DisplayNotificationTask.java
@@ -1,7 +1,7 @@
 package io.invertase.firebase.notifications;
 
 import android.app.Notification;
-import android.app.NotificationManager;
+import androidx.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
